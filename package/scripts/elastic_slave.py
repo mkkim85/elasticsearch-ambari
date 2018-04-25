c2136ad1d8460440f199c07ffaffe2d8286eb7e8
mkkim <mkkim85y@gmail.com>
2018-04-25 13:46:38 +0900

    Update elastic_slave.py

--- package/scripts/elastic_slave.py
+++ package/scripts/elastic_slave.py
@@ -29,12 +29,14 @@ class Elasticsearch(Script):
         env.set_params(params)
         print 'Install the Slave'
         Execute('rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch')
-        Execute("echo \"[elasticsearch-2.x]\n"
+        Execute("echo \"[elasticsearch-6.x]\n"
                 "name=Elasticsearch repository for 6.x packages\n"
                 "baseurl=https://artifacts.elastic.co/packages/6.x/yum\n"
                 "gpgcheck=1\n"
                 "gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch\n"
-                "enabled=1\" > /etc/yum.repos.d/elasticsearch.repo")
+                "enabled=1\n"
+                "autorefresh=1\n"
+                "type=rpm-md\" > /etc/yum.repos.d/elasticsearch.repo")
         self.install_packages(env)
 
     def configure(self, env, upgrade_type=None, config_dir=None):
